5f2a9eb755786f2d52f8b06faf4c591de6fec24e
Dahmane Abdelmalek <dahmane.abdelmalek13@gmail.com>
2022-08-20 15:08:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Express = require("express");
+const app = Express();
+const cors = require("cors");
+require("dotenv").config();
+const { createServer } = require("http");
+const httpServer = createServer(app);
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+app.use(Express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(Express.json());
+
+httpServer.listen(process.env.APP_PORT, () => {
+  console.log("app running at port " + process.env.APP_PORT);
+});
