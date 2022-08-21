236ee45639ebf667842e0e6057edc7c6d541d674
Dahmane Abdelmalek <dahmane.abdelmalek13@gmail.com>
2022-08-21 12:19:16 +0100

    routes update

--- app/routes/files.routes.js
+++ app/routes/files.routes.js
@@ -4,16 +4,11 @@ const router = require("express").Router();
 router.get("/files/:id", async (req, res) => {
   const { id } = req.params;
   console.log(id, "suck");
-  drive.files.list({ fields: `files(${id})` }, (err, res) => {
+  drive.files.list({ q: `'${id}' in parents` }, (err, res) => {
     if (err) throw err;
     const files = res.data.files;
-    if (files.length) {
-      files.map((file) => {
-        console.log(file);
-      });
-    } else {
-      console.log("No files found");
-    }
+
+    console.log(res.data);
   });
 });
 
