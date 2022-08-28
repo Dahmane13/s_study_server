ef014b70596c93b5e306a0ef7371c42ce1ebbd23
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-08-28 11:46:29 +0100

    add: handle search files&folders By name
    - parents[]	: the IDs of the parent folders which contain the file.

--- app/routes/files.routes.js
+++ app/routes/files.routes.js
@@ -3,12 +3,56 @@ const router = require("express").Router();
 
 router.get("/files/:id", async (req, res) => {
   const { id } = req.params;
-  drive.files.list({ q: `'${id}' in parents` }, (err, response) => {
-    if (err) throw err;
-    // console.log(response.data);
-    const { files } = response.data;
-    res.json(files);
-  });
+
+  if (typeof id === "undefined" || !id || id === null) {
+    res.send("Invalid Id !");
+  } else {
+    drive.files.list({ q: `'${id}' in parents` }, (err, response) => {
+      if (err) throw err;
+      // console.log(response.data);
+      const { files } = response.data;
+      res.json(files);
+    });
+  }
+});
+
+// GET /query/:filename
+// Search for specific files or folders on the current user's My Drive BY NAME
+//  return reponse example
+// [
+//   {
+//     id: "18WaQUGVwpWbuUPR37whI1mi9qtLja1nP",
+//     name: "TPs",
+//     parents: ["1NjP4tBvyoBwELikKoH4d0R-Ii1oSpGHt"],
+//   },
+//   {
+//     id: "1Ddyg5B7B05LokLb7hS5-xy0W4hAcSE5o",
+//     name: "TPs",
+//     parents: ["1ELbCUOd5nmucAcmkz7HeXfj8jMux2kRy"],
+//   },
+// ]
+
+router.get("/query/:filename", async (req, res) => {
+  const { filename } = req.params;
+
+  if (typeof filename === "undefined" || !filename || filename === null) {
+    res.send("Invalid name !");
+  } else {
+    drive.files.list(
+      {
+        q: `name = '${filename}'`,
+        fields: "files(id,name,parents)",
+      },
+      (err, response) => {
+        if (err) throw err;
+        // console.log(response.data);
+        const { files } = response.data;
+
+        console.log(`name = '${filename}'`, files);
+        res.json(files);
+      }
+    );
+  }
 });
 
 module.exports = router;
